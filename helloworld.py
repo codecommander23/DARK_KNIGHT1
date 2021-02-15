028fb12248ad53f6775a832572e52df1ae355085
namanmadmax <64856329+namanmadmax@users.noreply.github.com>
2021-02-15 12:27:59 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+print('Hello,World!')
+print(1+2)
+print(7*6)
+print()
+print("The end","or is it?","keep watching to learn more about Python",3)
+print("Terry\tJohn\tGraham\tMichael\tEric\tTerry")
+first_name = "John"
+last_name = "Cleese"
+age = 78
+
+print(first_name + last_name + age)
+
+
+
+
+
+
